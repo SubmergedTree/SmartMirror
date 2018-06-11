107a4c18963cc230b5bc220a73bcac21358ec020
SubmergedTree <jannmann1@web.de>
2018-06-11 21:21:20 +0200

    register function

--- js/chuckNorisEval.js
+++ js/chuckNorisEval.js
@@ -1,10 +1,10 @@
-registrationsAfterRestCall.push(new Registration("ChuckNorisJoke", chuckNorisEval()));
+register("ChuckNorisJoke", chuckNorisEval());
 
-function chuckNorisEval(url, position, widgetType, queryString) {
-    //changeWidget(position, chuckNorisHtml);
-    restCall(url, chuckNorisCallback, position, queryString)
+function chuckNorisEval(url, position, widgetType, context) {
+    restCall(url, chuckNorisCallback, position, queryString);
 }
 
 function chuckNorisCallback(json, position) {
-
+    const formatedHtml = chuckNorisHtml.format(json.value.joke);
+    changeWidget(position, formatedHtml);
 }
