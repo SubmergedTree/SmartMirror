52bf072f82b02f06e459a62b83b7b462568cfd9a
SubmergedTree <jannmann1@web.de>
2018-06-11 21:09:40 +0200

    string format method

--- js/chuckNorisEval.js
+++ js/chuckNorisEval.js
@@ -1,3 +1,10 @@
-function chuckNorisEval(result, position) {
-    changeWidget(position, chuckNorisHtml);
+registrationsAfterRestCall.push(new Registration("ChuckNorisJoke", chuckNorisEval()));
+
+function chuckNorisEval(url, position, widgetType, queryString) {
+    //changeWidget(position, chuckNorisHtml);
+    restCall(url, chuckNorisCallback, position, queryString)
+}
+
+function chuckNorisCallback(json, position) {
+
 }
