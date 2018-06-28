333d799b2d757c4db6217720a9a1a651a3bb5094
SubmergedTree <jannmann1@web.de>
2018-06-28 22:26:28 +0200

    widget loader: test stubs

--- loader/load_widgets.py
+++ loader/load_widgets.py
@@ -1,25 +1,22 @@
-from util.path import get_files_in_dir
-
-JS_FILENAME_END = 'Eval.js'
-
 
 class WidgetLoader:
-    def __init__(self, WidgetDao, js_dir_path):
-        self.__widget_dao = WidgetDao
-        self.__js_dir_path = js_dir_path
+    def __init__(self, widget_dao, url_mapping):
+        self.__widget_dao = widget_dao
+        self.__url_mapping = url_mapping
 
     def update_widget_table(self):
-        js_files = get_files_in_dir(self.__js_dir_path)
         current_widgets_in_table = self.__widget_dao.get_widgets()
-        widgets_in_dir = []
-        for file in js_files:
-            if file.endswith(JS_FILENAME_END):
-                widgets_in_dir.append(file[:-len(JS_FILENAME_END)])
 
-        for widget_in_table in current_widgets_in_table:
-            if widget_in_table not in widgets_in_dir:
-                self.__widget_dao.delete_widget(widget_in_table)
+        for item in self.__url_mapping:
+            if item.widget not in current_widgets_in_table:
+                self.__widget_dao.add_widget(item.widget, item.url)
 
-        for widget_in_dir in widgets_in_dir:
-            if widget_in_dir not in current_widgets_in_table:
-                self.__widget_dao.add_widget()
+        for widget in current_widgets_in_table:
+            if widget not in self.__get_widget_list_from_url_mapping():
+                self.__widget_dao.delete_widget(widget)
+
+    def __get_widget_list_from_url_mapping(self):
+        widget_list = []
+        for item in self.__url_mapping:
+            widget_list.append(item.widget)
+        return widget_list
---
+++ tests/widget_loader_test.py
@@ -0,0 +1,16 @@
+import unittest
+from loader.load_widgets import WidgetLoader
+from loader.load_urls import UrlMapping
+
+# TODO
+class TestWidgetLoader(unittest.TestCase):
+
+    def test_add_widget(self):
+        url_mapping = []
+        widget_loader = WidgetLoader()
+
+    def test_delete_widget(self):
+        pass
+
+    def test_add_and_delete_widget(self):
+        pass
